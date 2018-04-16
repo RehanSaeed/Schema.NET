9a2f7e058f347ecb21d48231ed3a7a81c7583fef
Pedro Sampaio <pedro.sampaio@faro.com>
2018-04-16 16:07:37 +0100

    Adding passing unit test for unknown schema.net types.

---
+++ Tests/Schema.NET.Test/ThingTest.cs
@@ -0,0 +1,32 @@
+namespace Schema.NET.Test
+{
+    using System;
+    using Newtonsoft.Json;
+    using Xunit;
+
+    public class ThingTest
+    {
+        private Thing thing = new Thing()
+        {
+            Name = "New Object",
+            Description = "This is the description of a new object we can't deserialize",
+            Image = new Uri("https://example.com/image.jpg")
+        };
+
+        private readonly string json =
+            "{" +
+            "\"@context\":\"http://schema.org\"," +
+            "\"@type\":\"NewObject\"," +
+            "\"name\":\"New Object\"," +
+            "\"description\":\"This is the description of a new object we can't deserialize\"," +
+            "\"image\":\"https://example.com/image.jpg\"," +
+            "\"someProperty\":\"not supported\"" +
+            "}";
+
+        [Fact]
+        public void Deserializing_NewObjectJsonLd_ReturnsThing()
+        {
+            Assert.Equal(this.thing.ToString(), JsonConvert.DeserializeObject<Thing>(this.json).ToString());
+        }
+    }
+}
