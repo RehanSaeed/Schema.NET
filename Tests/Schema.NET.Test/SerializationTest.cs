2d57b83187c4a224114151e9ccb05bfc933f4e38
Ben McCallum <ben.mccallum@live.com.au>
2018-10-12 10:27:47 +0200

    Update custom serializer test so it doesn't break as new props are added to the local business type.

--- Tests/Schema.NET.Test/SerializationTest.cs
+++ Tests/Schema.NET.Test/SerializationTest.cs
@@ -52,26 +52,17 @@ namespace Schema.NET.Test
             {
                 new StringEnumConverter()
             },
-            NullValueHandling = NullValueHandling.Include, // changed
+            NullValueHandling = NullValueHandling.Ignore,
             StringEscapeHandling = StringEscapeHandling.EscapeHtml,
+            Formatting = Formatting.Indented
         };
 
         private readonly string jsonCustom =
-        "{" +
-            "\"@context\":\"http://schema.org\"," +
-            "\"@type\":\"Person\"," +
-            "\"@id\":null," +
-            "\"name\":\"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"," +
-            "\"description\":null,\"additionalType\":null,\"alternateName\":null,\"disambiguatingDescription\":null,\"identifier\":null," +
-            "\"image\":null,\"mainEntityOfPage\":null,\"potentialAction\":null,\"sameAs\":null,\"url\":null,\"additionalName\":null," +
-            "\"address\":null,\"affiliation\":null,\"alumniOf\":null,\"award\":null,\"birthDate\":null,\"birthPlace\":null,\"brand\":null," +
-            "\"children\":null,\"colleague\":null,\"contactPoint\":null,\"deathDate\":null,\"deathPlace\":null,\"duns\":null,\"email\":null," +
-            "\"familyName\":null,\"faxNumber\":null,\"follows\":null,\"funder\":null,\"gender\":null,\"givenName\":null," +
-            "\"globalLocationNumber\":null,\"hasOfferCatalog\":null,\"hasPOS\":null,\"height\":null,\"homeLocation\":null," +
-            "\"honorificPrefix\":null,\"honorificSuffix\":null,\"isicV4\":null,\"jobTitle\":null,\"knows\":null,\"makesOffer\":null," +
-            "\"memberOf\":null,\"naics\":null,\"nationality\":null,\"netWorth\":null,\"owns\":null,\"parent\":null,\"performerIn\":null," +
-            "\"publishingPrinciples\":null,\"relatedTo\":null,\"seeks\":null,\"sibling\":null,\"sponsor\":null,\"spouse\":null,\"taxID\":null," +
-            "\"telephone\":null,\"vatID\":null,\"weight\":null,\"workLocation\":null,\"worksFor\":null}";
+        "{\r\n" +
+            "  \"@context\": \"http://schema.org\",\r\n" +
+            "  \"@type\": \"Person\",\r\n" +
+            "  \"name\": \"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"\r\n" +
+        "}";
 
         [Fact]
         public void ToString_UnsafeBookData_ReturnsExpectedJsonLd() =>
