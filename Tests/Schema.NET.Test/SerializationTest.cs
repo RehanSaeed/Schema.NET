29f3dfaff25dd14432361eef920110ec52e80d06
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-11-05 10:14:37 +0000

    Fix test on linux

--- Tests/Schema.NET.Test/SerializationTest.cs
+++ Tests/Schema.NET.Test/SerializationTest.cs
@@ -9,6 +9,37 @@ namespace Schema.NET.Test
     // https://developers.google.com/search/docs/data-types/books
     public class SerializationTest
     {
+        private const string Json =
+            "{" +
+                "\"@context\":\"http://schema.org\"," +
+                "\"@type\":\"Book\"," +
+                "\"@id\":\"http://example.com/book/1\"," +
+                "\"name\":\"The Catcher in the Rye</script><script>alert('gotcha');</script>\"," +
+                "\"author\":{" +
+                    "\"@type\":\"Person\"," +
+                    "\"name\":\"J.D. Salinger</script><script>alert('gotcha');</script>\"" +
+                "}" +
+            "}";
+
+        private const string JsonHtmlEscaped =
+            "{" +
+                "\"@context\":\"http://schema.org\"," +
+                "\"@type\":\"Book\"," +
+                "\"@id\":\"http://example.com/book/1\"," +
+                "\"name\":\"The Catcher in the Rye\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"," +
+                "\"author\":{" +
+                    "\"@type\":\"Person\"," +
+                    "\"name\":\"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"" +
+                "}" +
+            "}";
+
+        private const string JsonCustom =
+            "{\r\n" +
+                "  \"@context\": \"http://schema.org\",\r\n" +
+                "  \"@type\": \"Person\",\r\n" +
+                "  \"name\": \"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"\r\n" +
+            "}";
+
         private static readonly Person Person = new Person()
         {
             Name = "J.D. Salinger</script><script>alert('gotcha');</script>",
@@ -22,30 +53,6 @@ namespace Schema.NET.Test
             Author = Person
         };
 
-        private readonly string json =
-        "{" +
-            "\"@context\":\"http://schema.org\"," +
-            "\"@type\":\"Book\"," +
-            "\"@id\":\"http://example.com/book/1\"," +
-            "\"name\":\"The Catcher in the Rye</script><script>alert('gotcha');</script>\"," +
-            "\"author\":{" +
-                "\"@type\":\"Person\"," +
-                "\"name\":\"J.D. Salinger</script><script>alert('gotcha');</script>\"" +
-            "}" +
-        "}";
-
-        private readonly string jsonHtmlEscaped =
-        "{" +
-            "\"@context\":\"http://schema.org\"," +
-            "\"@type\":\"Book\"," +
-            "\"@id\":\"http://example.com/book/1\"," +
-            "\"name\":\"The Catcher in the Rye\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"," +
-            "\"author\":{" +
-                "\"@type\":\"Person\"," +
-                "\"name\":\"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"" +
-            "}" +
-        "}";
-
         private readonly JsonSerializerSettings customSerializerSettings = new JsonSerializerSettings()
         {
             Converters = new List<JsonConverter>()
@@ -57,24 +64,19 @@ namespace Schema.NET.Test
             Formatting = Formatting.Indented
         };
 
-        private readonly string jsonCustom =
-        "{\r\n" +
-            "  \"@context\": \"http://schema.org\",\r\n" +
-            "  \"@type\": \"Person\",\r\n" +
-            "  \"name\": \"J.D. Salinger\\u003c/script\\u003e\\u003cscript\\u003ealert(\\u0027gotcha\\u0027);\\u003c/script\\u003e\"\r\n" +
-        "}";
-
         [Fact]
         public void ToString_UnsafeBookData_ReturnsExpectedJsonLd() =>
-            Assert.Equal(this.json, this.book.ToString());
+            Assert.Equal(Json, this.book.ToString());
 
         [Fact]
         public void ToHtmlEscapedString_UnsafeBookData_ReturnsExpectedJsonLd() =>
-            Assert.Equal(this.jsonHtmlEscaped, this.book.ToHtmlEscapedString());
+            Assert.Equal(JsonHtmlEscaped, this.book.ToHtmlEscapedString());
 
         [Fact]
         public void ToStringWithCustomSerializerSettings_UnsafeAuthorData_ReturnsExpectedJsonLd() =>
-            Assert.Equal(this.jsonCustom, Person.ToString(this.customSerializerSettings));
+            Assert.Equal(
+                JsonCustom.Replace("\r\n", Environment.NewLine),
+                Person.ToString(this.customSerializerSettings));
 
         [Fact]
         public void ToStringWithNullAssignedProperty_ReturnsExpectedJsonLd()
