06a9f20b10962d39d96e3dce960be7503c8a7dc4
Andre <andre.steenbergen@gmail.com>
2019-02-06 12:00:46 +0100

    Discarded change to webpage element

--- Source/Schema.NET/core/WebPageElement.cs
+++ Source/Schema.NET/core/WebPageElement.cs
@@ -15,19 +15,5 @@ namespace Schema.NET
         /// </summary>
         [DataMember(Name = "@type", Order = 1)]
         public override string Type => "WebPageElement";
-
-        /// <summary>
-        /// A CSS selector, e.g. of a &lt;a class="localLink" href="http://schema.org/SpeakableSpecification"&gt;SpeakableSpecification&lt;/a&gt; or &lt;a class="localLink" href="http://schema.org/WebPageElement"&gt;WebPageElement&lt;/a&gt;. In the latter case, multiple matches within a page can constitute a single conceptual "Web page element".
-        /// </summary>
-        [DataMember(Name = "cssSelector", Order = 206)]
-        [JsonConverter(typeof(ValuesConverter))]
-        public OneOrMany<CssSelectorType>? CssSelector { get; set; }
-
-        /// <summary>
-        /// An XPath, e.g. of a &lt;a class="localLink" href="http://schema.org/SpeakableSpecification"&gt;SpeakableSpecification&lt;/a&gt; or &lt;a class="localLink" href="http://schema.org/WebPageElement"&gt;WebPageElement&lt;/a&gt;. In the latter case, multiple matches within a page can constitute a single conceptual "Web page element".
-        /// </summary>
-        [DataMember(Name = "xpath", Order = 207)]
-        [JsonConverter(typeof(ValuesConverter))]
-        public OneOrMany<XPathType>? Xpath { get; set; }
     }
 }
