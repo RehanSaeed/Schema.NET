b96c885b0a6dc49d676ccddda1bb7bef058d58ce
DEV-USSEAWK06\Kirk Marple <kirk@candela.software>
2019-06-22 18:05:47 -0700

    no message

---
+++ Tests/Schema.NET.Test/core/TestDefaults.cs
@@ -0,0 +1,22 @@
+namespace Schema.NET.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    public static class TestDefaults
+    {
+        public static readonly JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings()
+        {
+            Formatting = Formatting.None,
+            DateParseHandling = DateParseHandling.DateTimeOffset,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.None,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+    }
+}
