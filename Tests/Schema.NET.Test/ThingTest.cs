6a989a36ba9363cbc6cdd3b4ab58538f75940c3e
Turnerj <git@turnerj.com>
2019-12-15 14:34:47 +1030

    Add Thing equality tests

--- Tests/Schema.NET.Test/ThingTest.cs
+++ Tests/Schema.NET.Test/ThingTest.cs
@@ -28,5 +28,71 @@ namespace Schema.NET.Test
             Assert.Equal(
                 this.thing.ToString(),
                 JsonConvert.DeserializeObject<Thing>(this.json, TestDefaults.DefaultJsonSerializerSettings).ToString());
+
+        [Fact]
+        public void Equality_AreEqual_Default() => CompareEqual(new Thing(), new Thing());
+
+        [Fact]
+        public void Equality_AreEqual_SinglePropertyValue() => CompareEqual(
+            new Thing
+            {
+                Name = "Custom Name"
+            }, new Thing
+            {
+                Name = "Custom Name"
+            });
+
+        [Fact]
+        public void Equality_AreEqual_MultiPropertyValue() => CompareEqual(
+            new Thing
+            {
+                Name = "Custom Name",
+                Url = new Uri("https://schema.net")
+            }, new Thing
+            {
+                Name = "Custom Name",
+                Url = new Uri("https://schema.net")
+            });
+
+        [Fact]
+        public void Equality_AreNotEqual_Null() => CompareNotEqual(
+            new Thing
+            {
+                Name = "A"
+            }, null);
+
+        [Fact]
+        public void Equality_AreNotEqual_SinglePropertyValue() => CompareNotEqual(
+            new Thing
+            {
+                Name = "A"
+            }, new Thing
+            {
+                Name = "B"
+            });
+
+        [Fact]
+        public void Equality_AreNotEqual_MultiPropertyValue() => CompareNotEqual(
+            new Thing
+            {
+                Name = "A",
+                Url = new Uri("https://schema.net")
+            }, new Thing
+            {
+                Name = "B",
+                Url = new Uri("https://schema.net/Thing")
+            });
+
+        private static void CompareEqual<T>(T a, T b)
+        {
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+            Assert.True(a.Equals(b));
+        }
+
+        private static void CompareNotEqual<T>(T a, T b)
+        {
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+            Assert.False(a.Equals(b));
+        }
     }
 }
