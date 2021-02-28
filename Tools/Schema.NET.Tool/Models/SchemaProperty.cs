69ec925506b8b47257087ec4d3d93b5d61a3bdad
Turnerj <git@turnerj.com>
2021-02-28 14:24:15 +1030

    Moving some shared properties around and fixing some bugs

--- Tools/Schema.NET.Tool/Models/SchemaProperty.cs
+++ Tools/Schema.NET.Tool/Models/SchemaProperty.cs
@@ -2,17 +2,10 @@ namespace Schema.NET.Tool.Models
 {
     using System;
     using System.Collections.Generic;
-    using Schema.NET.Tool.Constants;
 
     public class SchemaProperty : SchemaObject
     {
         public List<Uri> DomainIncludes { get; } = new List<Uri>();
-
-        public bool IsArchived => string.Equals(this.Layer, LayerName.Archived, StringComparison.OrdinalIgnoreCase);
-
-        public bool IsMeta => string.Equals(this.Layer, LayerName.Meta, StringComparison.OrdinalIgnoreCase);
-
-        public bool IsPending => string.Equals(this.Layer, LayerName.Pending, StringComparison.OrdinalIgnoreCase);
 
         public List<Uri> RangeIncludes { get; } = new List<Uri>();
     }
--- Tools/Schema.NET.Tool/Repositories/ISchemaRepository.cs
+++ Tools/Schema.NET.Tool/Repositories/ISchemaRepository.cs
@@ -1,11 +1,12 @@
 namespace Schema.NET.Tool.Repositories
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Schema.NET.Tool.Models;
 
     public interface ISchemaRepository
     {
-        Task<(IEnumerable<SchemaClass> Classes, IEnumerable<SchemaProperty> Properties, IEnumerable<SchemaEnumerationValue> EnumerationValues)> GetObjectsAsync();
+        Task<(IEnumerable<SchemaClass> Classes, IEnumerable<SchemaProperty> Properties, IEnumerable<SchemaEnumerationValue> EnumerationValues)> GetObjectsAsync(Func<SchemaObject, bool> filter = null);
     }
 }
