267dcf076d5873b61b376b7be507ac8ac725b89d
Turnerj <git@turnerj.com>
2021-02-28 11:36:31 +1030

    Removing old generator code

--- Tools/Schema.NET.Tool/ViewModels/Property.cs
+++ Tools/Schema.NET.Tool/ViewModels/Property.cs
@@ -58,45 +58,6 @@ namespace Schema.NET.Tool.ViewModels
             }
         }
 
-        public void AppendIndentLine(StringBuilder stringBuilder, int indent)
-        {
-            stringBuilder.AppendCommentSummary(indent, this.Description);
-
-            var isVirtual = this
-                .Class
-                .Descendants
-                .Any(x => x.Properties.Any(y => string.Equals(y.Name, this.Name, StringComparison.Ordinal)));
-            var isOverride = this
-                .Class
-                .Ancestors
-                .Any(x => x.Properties.Any(y => string.Equals(y.Name, this.Name, StringComparison.Ordinal)));
-            var modifier = isVirtual ? " virtual" : string.Empty;
-            modifier = isOverride ? " override" : modifier;
-
-            stringBuilder.AppendIndentLine(indent, $"[DataMember(Name = \"{this.JsonName}\", Order = {this.Order})]");
-
-            if (this.Types.Any(x => string.Equals(x.Name, "Duration", StringComparison.OrdinalIgnoreCase)))
-            {
-                stringBuilder.AppendIndentLine(indent, "[JsonConverter(typeof(TimeSpanToISO8601DurationValuesJsonConverter))]");
-            }
-            else if (this.Types.Any(x => string.Equals(x.Name, "Date", StringComparison.OrdinalIgnoreCase)))
-            {
-                stringBuilder.AppendIndentLine(indent, "[JsonConverter(typeof(DateTimeToIso8601DateValuesJsonConverter))]");
-            }
-            else
-            {
-                stringBuilder.AppendIndentLine(indent, "[JsonConverter(typeof(ValuesJsonConverter))]");
-            }
-
-            stringBuilder.AppendIndentLine(indent, $"public{modifier} {this.PropertyTypeString} {this.Name} {{ get; set; }}");
-        }
-
-        public void AppendIndentLineForInterface(StringBuilder stringBuilder, int indent)
-        {
-            stringBuilder.AppendCommentSummary(indent, this.Description);
-            stringBuilder.AppendIndentLine(indent, $"{this.PropertyTypeString} {this.Name} {{ get; set; }}");
-        }
-
         public Property Clone()
         {
             var property = new Property()
