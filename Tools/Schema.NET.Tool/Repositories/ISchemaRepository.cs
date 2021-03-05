75b468fc22afc118587e88a8a8ae8678d48777d1
Turnerj <git@turnerj.com>
2021-03-05 23:18:28 +1030

    Updated pending check logic
    Note: Pending properties are no longer being generated

--- Tools/Schema.NET.Tool/Repositories/ISchemaRepository.cs
+++ Tools/Schema.NET.Tool/Repositories/ISchemaRepository.cs
@@ -1,12 +1,11 @@
 namespace Schema.NET.Tool.Repositories
 {
-    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Schema.NET.Tool.Models;
 
     public interface ISchemaRepository
     {
-        Task<(IEnumerable<SchemaClass> Classes, IEnumerable<SchemaProperty> Properties, IEnumerable<SchemaEnumerationValue> EnumerationValues)> GetObjectsAsync(Func<SchemaObject, bool> filter = null);
+        Task<(IEnumerable<SchemaClass> Classes, IEnumerable<SchemaProperty> Properties, IEnumerable<SchemaEnumerationValue> EnumerationValues)> GetObjectsAsync();
     }
 }
