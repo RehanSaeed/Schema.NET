85dde5a5605bd2449358949a6306aaf537cf2d0b
Turnerj <git@turnerj.com>
2021-03-17 13:18:23 +1030

    Nullable reference type fixes for PropertyValueSpecification

--- Source/Schema.NET/PropertyValueSpecification.Partial.cs
+++ Source/Schema.NET/PropertyValueSpecification.Partial.cs
@@ -1,7 +1,7 @@
 namespace Schema.NET
 {
+    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     /// <summary>
     /// A Property value specification.
@@ -17,57 +17,38 @@ namespace Schema.NET
         /// </returns>
         public override string ToString()
         {
-            var stringBuilder = new StringBuilder();
+            var values = new List<string>();
 
-            if (this.ValueMaxLength.First().HasValue)
+            if (this.ValueMaxLength.First() is double maxLength)
             {
-                stringBuilder.Append("maxlength=");
-                stringBuilder.Append(this.ValueMaxLength.First().Value);
+                values.Add($"maxlength={maxLength}");
             }
 
-            if (this.ValueMinLength.First().HasValue)
+            if (this.ValueMinLength.First() is double minLength)
             {
-                AppendSpace(stringBuilder);
-                stringBuilder.Append("minlength=");
-                stringBuilder.Append(this.ValueMinLength.First().Value);
+                values.Add($"minlength={minLength}");
             }
 
-            if (this.ValueName.First() is not null)
+            if (this.ValueName.First() is string name)
             {
-                AppendSpace(stringBuilder);
-                stringBuilder.Append("name=");
-                stringBuilder.Append(this.ValueName.First());
+                values.Add($"name={name}");
             }
 
-            if (this.ValuePattern.First() is not null)
+            if (this.ValuePattern.First() is string pattern)
             {
-                AppendSpace(stringBuilder);
-                stringBuilder.Append("pattern=");
-                stringBuilder.Append(this.ValuePattern.First());
+                values.Add($"pattern={pattern}");
             }
 
-            if (this.ValuePattern.First() is object)
+            if (this.ValueRequired.First() is true)
             {
-                AppendSpace(stringBuilder);
-                stringBuilder.Append("pattern=");
-                stringBuilder.Append(this.ValuePattern.First());
+                values.Add("required");
             }
 
-            if (this.ValueRequired.First().HasValue)
-            {
-                AppendSpace(stringBuilder);
-                stringBuilder.Append("required");
-            }
-
-            return stringBuilder.ToString();
-        }
-
-        private static void AppendSpace(StringBuilder stringBuilder)
-        {
-            if (stringBuilder.Length > 0)
-            {
-                stringBuilder.Append(' ');
-            }
+#if NETSTANDARD2_0 || NET472 || NET461
+            return string.Join(" ", values);
+#else
+            return string.Join(' ', values);
+#endif
         }
     }
 }
