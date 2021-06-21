c48b431192dccf39e8443516a232270d25dab395
Turnerj <git@turnerj.com>
2021-06-21 21:16:50 +0930

    Added basic updater tool

---
+++ Tools/Schema.NET.Updater/Program.cs
@@ -0,0 +1,27 @@
+namespace Schema.NET.Updater
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Updates the local Schema JSON file with data from Schema.org.
+    /// </summary>
+    internal class Program
+    {
+        private const string SchemaJsonSourceUrl = "https://schema.org/version/latest/schemaorg-all-https.jsonld";
+        private const string SchemaJsonDestinationFilePath = "../../Data/schemaorg-all-https.jsonld";
+
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Downloading '{0}'...", SchemaJsonSourceUrl);
+            var httpClient = new HttpClient();
+            using var stream = await httpClient.GetStreamAsync(SchemaJsonSourceUrl).ConfigureAwait(true);
+            Console.WriteLine("Saving to '{0}'...", SchemaJsonDestinationFilePath);
+            using var fileStream = File.Open(SchemaJsonDestinationFilePath, FileMode.Create);
+            await stream.CopyToAsync(fileStream).ConfigureAwait(true);
+            Console.WriteLine("Update complete!");
+        }
+    }
+}
