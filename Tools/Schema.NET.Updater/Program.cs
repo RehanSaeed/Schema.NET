d3a9418c5bc57de2ce763eead6c497b647860ed9
Turnerj <git@turnerj.com>
2023-05-17 21:59:47 +0930

    Upgrade to Schema.org v16

--- Tools/Schema.NET.Updater/Program.cs
+++ Tools/Schema.NET.Updater/Program.cs
@@ -3,6 +3,7 @@ namespace Schema.NET.Updater;
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 
 /// <summary>
@@ -12,6 +13,7 @@ internal sealed class Program
 {
     private const string SchemaJsonSourceUrl = "https://schema.org/version/latest/schemaorg-all-https.jsonld";
     private const string SchemaJsonDestinationFilePath = "../../Data/schemaorg-all-https.jsonld";
+    private const string ProjectDirectoryAttributeKey = "ProjectDirectory";
 
     private static async Task Main()
     {
@@ -22,9 +24,23 @@ internal sealed class Program
 
         Console.WriteLine(Resources.SavingTo, SchemaJsonDestinationFilePath);
 
-        using var fileStream = File.Open(SchemaJsonDestinationFilePath, FileMode.Create);
+        var fullSavePath = Path.Combine(GetProjectDirectory() ?? string.Empty, SchemaJsonDestinationFilePath);
+        using var fileStream = File.Open(fullSavePath, FileMode.Create);
         await stream.CopyToAsync(fileStream).ConfigureAwait(true);
 
         Console.WriteLine(Resources.UpdateComplete);
+    }
+
+    private static string? GetProjectDirectory()
+    {
+        foreach (var attr in Attribute.GetCustomAttributes(typeof(Program).Assembly))
+        {
+            if (attr is AssemblyMetadataAttribute metadataAttribute && metadataAttribute.Key == ProjectDirectoryAttributeKey)
+            {
+                return metadataAttribute.Value;
+            }
+        }
+
+        return null;
     }
 }
